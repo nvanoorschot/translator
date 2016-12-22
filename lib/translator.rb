e93d615415461e0a0af2123c2aaae85af34bc0f1
Nicke van Oorschot <nvanoorschot@forecastxl.com>
2016-12-22 14:54:10 +0100

    Start with loading module

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,3 @@
-require 'translator/i18n_patch'
-require 'translator/translation'
-require 'translator/reset'
-
-if defined?(Rails)
-  require 'translator/engine'
-  require 'translator/routes'
-  require 'translator/translations_controller'
-end
-
 module Translator
   # Disable the ability to pass default translations to the I18n.translate function.
   mattr_accessor :disable_default_translations
@@ -17,4 +7,14 @@ module Translator
   def self.setup
     yield self
   end
+end
+
+require 'translator/i18n_patch'
+require 'translator/translation'
+require 'translator/reset'
+
+if defined?(Rails)
+  require 'translator/engine'
+  require 'translator/routes'
+  require 'translator/translations_controller'
 end
